2f7842e80d7671ccba208daf09da0a7e01e659f5
afrateksystems <afrateksystems@gmail.com>
2026-04-02 12:11:03 +0530

    notes-app using react

---
+++ notes-app/src/components/NoteForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function NoteForm({ addNote}){
+    const [note,setNote] = useState({
+        title:"my note",
+        status:"created"
+    });
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        //if(!note.trim()) return;
+        addNote(note);
+        //setNote("");
+    };
+    return(
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Enter note" value={note.title} onChange={(e) => setNote({title:e.target.value})}/>
+            <input value={note.status} onChange={(e) => setNote({title:e.target.value})} />
+            <button>Add</button>        
+        </form>
+    );
+}
+
+export default NoteForm;
