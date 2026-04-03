765b0c49bf666621d793ee4551aa0e7da1cc68c7
afrateksystems <afrateksystems@gmail.com>
2026-04-03 09:46:12 +0530

    integrated the front end with axios

--- notes-app/src/components/NoteForm.js
+++ notes-app/src/components/NoteForm.js
@@ -1,21 +1,32 @@
+import axios from "axios";
 import { useState } from "react";
  
 function NoteForm({ addNote}){
     const [note,setNote] = useState({
         title:"note 1",
+        content:"",
         status:"open"
     });
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         //if(!note.trim()) return;
+        sendPostRequest(note);
         addNote(note);
         //setNote("");
     };
+    const sendPostRequest = (note)=>{
+     axios.post("http://localhost:3001/notes",note,{
+        headers:{
+            "content-type":"application/json",
+        },
+    })
+    }
     return(
         <form onSubmit={handleSubmit}>
-            <input placeholder="Enter note"  value={note.title} onChange={(e) => setNote({...note,title:e.target.value})}/>
+            <input placeholder="Enter the title"  value={note.title} onChange={(e) => setNote({...note,title:e.target.value})}/>
            {/* <input value={note.status} onChange={(e) => setNote({...note,status:e.target.value})} /> */}
+            <input placeholder="enter the content" value={note.content} onChange={(e) => setNote({...note,content:e.target.value})}/>
             <label>status</label><input type="checkbox" onChange={(e) =>setNote({...note,status:e.target.checked ? "closed" : "open"})}/>
             <button>Add</button>        
         </form>
