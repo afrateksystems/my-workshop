5bccaa88a50738d9aa95dfbde2430f3239ccf476
afrateksystems <afrateksystems@gmail.com>
2026-04-03 09:47:09 +0530

    implemented cors in the backend

--- notes-api/server.js
+++ notes-api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const notesRoutes = require('./routes/notesRoutes');
 //console.log('errrrrror##',notesRoutes);
+app.use(cors());
 app.use(express.json());
 app.use('/notes',notesRoutes);
 
