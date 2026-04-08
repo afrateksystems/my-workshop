1d749fde9ccd0701b37f31a4dbfddc398ae711be
afrateksystems <afrateksystems@gmail.com>
2026-04-08 11:46:44 +0530

    hackathon 2 front end and backend question-app

--- notes-app/src/components/NoteList.js
+++ notes-app/src/components/NoteList.js
@@ -1,21 +1,23 @@
 import NoteItem from "./NoteItem";
-import React, { useState } from "react";
+import StatusBarGraph from "./StatusBarGraph";
+import React, { useState, useEffect } from "react";
 function NoteList({notes,deleteNote}){
-    const [searchInput, setSearchInput] = useState(""); 
+    const [searchInput, setSearchInput] = useState("");
     const [filteredNotes, setFilteredNotes] = useState(notes);
-    const handleSearch = () => {
-        const result = notes.filter((note) =>note.title.toLowerCase().includes(searchInput.toLowerCase()) ||
-        note.content.toLowerCase().includes(searchInput.toLowerCase())
-    );
-    setFilteredNotes(result);
-  };
+    useEffect(() => {
+        const result = notes.filter(
+            (note) => note.title.toLowerCase().includes(searchInput.toLowerCase()) || note.content.toLowerCase().includes(searchInput.toLowerCase()));
+            setFilteredNotes(result);
+        }, [notes, searchInput]);
+    const displayedNotes = [...filteredNotes].sort((a, b) => a.priority - b.priority);
 
     return (
          <div>
-            <div className="searchtask">
             <input type='text' placeholder='searh tasks' className="searchbox"value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
-            <button onClick={handleSearch} className="search-btn">searchQuery</button>
-            </div>
+            {/* <button onClick={handleSearch} className="search-btn">searchQuery</button> */}
+            <div className="graph" style={{ width: '100%', maxWidth: 600, margin: '0 auto', display: 'flex', justifyContent: 'center' }}>
+  <StatusBarGraph notes={filteredNotes} />
+</div>
         <table style={{ width: "100%", borderCollapse: "collapse" }}>
             
             <thead>
@@ -24,14 +26,14 @@ function NoteList({notes,deleteNote}){
                     <th style={{border:"1px solid black",padding: "8px"}}>Content</th>
                     <th style={{border:"1px solid black",padding: "8px"}}>Date</th>
                     <th style={{border:"1px solid black",padding: "8px"}}>Time</th>
+                    <th style={{border:"1px solid black",padding: "8px"}}>Status</th>
                     <th style={{border:"1px solid black",padding: "8px"}}>Priority</th>
                     <th style={{border:"1px solid black",padding: "8px"}}>Actions</th>
                 </tr>
             </thead>
         
            <tbody>
-
-          {(searchInput ? filteredNotes : notes).map((note) => (
+            {displayedNotes.map((note) => (
             <NoteItem key={note.id} note={note} deleteNote={deleteNote} />
           ))}
         </tbody>
