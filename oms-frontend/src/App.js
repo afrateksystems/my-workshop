c3200e55af2652a63b83fd2993f846eacd4a3502
afrateksystems <afrateksystems@gmail.com>
2026-04-15 11:29:45 +0530

    front end of oms application

---
+++ oms-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'; 
+import OrderForm from './component/orderform';
+
+function App() {
+  return (
+    <div >
+       <OrderForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ oms-frontend/src/component/orderform.js
@@ -0,0 +1,58 @@
+import React,{useState} from "react";
+
+const OrderForm = () =>{
+    const [item,setItem] = useState("");
+    const [price,setPrice] = useState("");
+    const [orderLines,setOrderLines] = useState([]);
+    const handleSubmit = async () => {
+    try {
+        const response = await fetch("http://localhost:8080/order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                orderLines: orderLines
+            })
+        });
+        const result = await response.json();
+        console.log("Order saved with ID:", result);
+        setOrderLines([]);
+        setItem("");
+        setPrice("");
+    } catch (error) {
+        console.error("Error submitting order:", error);
+    }
+};
+    const handleAddItem =() =>{
+        if (!item || !price) return;
+        const newLine = {
+            item,
+            price
+        };
+        setOrderLines([...orderLines, newLine]);
+        //we need to clear inputs
+        setItem("");
+        setPrice("");
+    }
+    return(
+        <div className="container">
+            <h2>Create Your Order</h2>
+            <div className="input-row">
+            <input type="text" placeholder="item" value={item} onChange={(e) => setItem(e.target.value)}></input>
+            <input type="number" placeholder="price" value={price} onChange={(e) => setPrice(e.target.value)}></input>
+            </div>
+            <button className="add-btn" onClick={handleAddItem}>Add Item</button>
+            <ul>
+                {orderLines.map((line, index) => (
+                    <li key={index}>
+                        {line.item} - ₹{line.price}
+                    </li>
+                ))}
+            </ul>
+            <button className="submit-btn" onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+
+};
+export default OrderForm;
